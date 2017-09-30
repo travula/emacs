60c093cff6e6c6951eeabda8998d8a52bb222c2c
Venkatesh Choppella <choppell@gmail.com>
2017-09-30 23:22:15 +0530

    deleted unneeded libraries under lisp/other

--- src/lisp/other/bmacs/bmacs-config.el
+++
@@ -1,6 +0,0 @@
-;; Automatically generated file (don't edit) */
-;; Fri Aug  5 14:45:10 EDT 2005
-
-(provide 'bmacs-config)
-(defvar bmacs-docdir "/home/choppell/apps/bigloo2.6f/doc/bigloo-2.6f")
-(defvar bmacs-lispdir "/usr/share/emacs/21.3/site-lisp/bigloo")
